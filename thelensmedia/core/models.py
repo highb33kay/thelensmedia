de27e72e9629929374a41da4277bf97c54743509
highb33kay <hiibeekayvibe@gmail.com>
2022-11-21 14:00:12 +0100

    done with initial build

---
+++ thelensmedia/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    category = models.CharField(max_length=200, choices=CATEGORY)
+    description = models.CharField(max_length=200, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
