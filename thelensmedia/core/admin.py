15003f17669ccf5cbd188752a26f47d0934cc2c4
highb33kay <hiibeekayvibe@gmail.com>
2022-11-25 00:17:37 +0100

    user database done

--- thelensmedia/core/admin.py
+++ thelensmedia/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from models import CustomerProfile, VendorProfile
 
 # Register your models here.
+
+# Register customer profile
+
+
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fname', 'lname', 'email')
+
+
+admin.site.register(CustomerProfile, CustomerProfileAdmin)
+
+# register vendor profile
+
+
+class VendorProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fname', 'lname', 'email', 'vendor_name',
+                    'bio', 'logo', 'created_at', 'updated_at', 'url')
+
+    prepopulated_fields = {'slug': ('vendor_name',)}
+
+
+admin.register(VendorProfile, VendorProfileAdmin)
